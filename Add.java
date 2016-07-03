612796bf4d7edf3eeb5699a039d5a3ef7c347eac
gokulandroid5 <gokul.brain18@gmail.com>
2016-07-03 13:49:27 +0530

    prog chanded
    changed

--- Add.java
+++ Add.java
@@ -1,10 +1,8 @@
-package sample1;
-
 public class Add {
 	public static void main(String[] args){
-		int a=10,b=5,c;
-		c=a+b;
-		System.out.println("a=10 b=5 total= "+c );
+		int a=10;
+		double c;
+	    float b=1.1f;
+		System.out.println("a="+a +" b="+b +" total= "+(a+b) );
 	}
-
 }
