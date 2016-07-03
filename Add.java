44932c056ab691e81049170ba18641cae4ad71d0
gokulandroid5 <gokulandroid5@users.noreply.github.com>
2016-07-03 12:50:55 +0530

    Update Add.java

--- Add.java
+++ Add.java
@@ -2,8 +2,8 @@ package sample1;
 
 public class Add {
 	public static void main(String[] args){
-		int a=10;
-		float b=5,c;
+		int a=10,c;
+		float b=5;
 		c=a+b;
 		System.out.println("a=10 b=5 total= "+c );
 	}
